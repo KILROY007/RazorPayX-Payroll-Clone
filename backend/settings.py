b12f8789d37eec5f506014e50f1e6638780124f2
KILROY007 <sumanthegde123@gmail.com>
2023-12-06 12:28:41 +0530

    allowed all hosts

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8j6ccg%)^(a6i6r551&+%r-qcz4--e*+t7-um9sg0tz5h0vfl+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
